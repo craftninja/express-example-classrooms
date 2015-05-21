95aefa1ba128db53db96d69f210fb5b7b9ed8648
EmilyPlatzer <emilyplatzer@gmail.com>
2015-05-21 11:41:28 -0600

    Can also render JSON

--- routes/classrooms.js
+++ routes/classrooms.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send('Classrooms index');
+  res.send({classrooms: 'Are awesome'});
 });
 
 module.exports = router;
