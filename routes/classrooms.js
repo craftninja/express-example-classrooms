7da4f16b9196dbe9087e5d614ba7f3320df32db6
EmilyPlatzer <emilyplatzer@gmail.com>
2015-05-21 11:46:26 -0600

    Render a template for classrooms, iteration on passed in array

--- routes/classrooms.js
+++ routes/classrooms.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.send({classrooms: 'Are awesome'});
+  // res.send({classrooms: 'Are awesome'});
+  var classroomData = ['Lecture Hall', 'Work Lab'];
+  res.render('classrooms/index', {classrooms: classroomData})
 });
 
 module.exports = router;
